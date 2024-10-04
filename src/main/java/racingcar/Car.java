cace3636ca486dbe42f0192228f72b961be8d4f3
seongkwang <ksg6736@jbnu.ac.kr>
2024-10-04 11:05:56 +0900

    feat: 자동차 이름 부여

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,8 @@
+package racingcar;
+
+public interface Car {
+    void setCarName(String name);
+    void setCarPosition(int num);
+    int getCarPosition();
+    String getCarName();
+}
---
+++ src/main/java/racingcar/RacingCar.java
@@ -0,0 +1,37 @@
+package racingcar;
+
+public class RacingCar implements Car{
+
+    private String carName;
+    private int carPosition;
+
+    public RacingCar(String name) {
+        this();
+        setCarName(name);
+    }
+
+    public RacingCar(){
+        carPosition = 0;
+    }
+
+    @Override
+    public void setCarName(String name) {
+        carName = name;
+    }
+
+    @Override
+    public void setCarPosition(int num) {
+        carPosition = num;
+    }
+
+    @Override
+    public int getCarPosition() {
+        return carPosition;
+    }
+
+    @Override
+    public String getCarName() {
+        return carName;
+    }
+
+}
