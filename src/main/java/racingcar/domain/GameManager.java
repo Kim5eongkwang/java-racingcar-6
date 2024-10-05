98a87ac8a889f702e109c5396fd29de53623ff32
Kim5eongKwang <ksg6736@jbnu.ac.kr>
2024-10-05 23:29:41 +0900

    feat: 게임 시작

--- src/main/java/racingcar/domain/GameManager.java
+++ src/main/java/racingcar/domain/GameManager.java
@@ -1,18 +1,46 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+import racingcar.ui.InputView;
+import racingcar.ui.OutputView;
+
 public class GameManager {
-    CarManager carManager;
+    private final CarManager carManager;
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final Judgement judgement;
 
     public GameManager() {
-        this.carManager = new CarManager();
+        carManager = new CarManager();
+        outputView = new OutputView();
+        inputView = new InputView(outputView);
+        judgement = new Judgement(carManager);
     }
 
     public void gameStart() {
+        List<String> carNames = inputView.readCarName();
 
+        addCars(carNames);
+
+        outputView.printResultMessage();
+        int moveCount = inputView.readMoveCount();
+        moveCars(moveCount, carManager.getCarList());
+
+        outputView.printWinner(judgement.getWinnerList());
     }
 
-    public void GameEnd() {
+    private void moveCars(int moveCount, List<Car> cars) {
+        for(int i = 0; i < moveCount; i++) {
+            carManager.timePass(() -> Randoms.pickNumberInRange(0,9));
+            outputView.printResult(cars);
+        }
+    }
 
+    private void addCars(List<String> carNames) {
+        for(String carName : carNames) {
+            carManager.addCar(new RacingCar(carName));
+        }
     }
 
 
